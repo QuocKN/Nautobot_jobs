98d7dbb797e0e20ebb9ca6b2d74cc40cc332a30a
Quoc Nguyen <quocnk2o4@gmail.com>
2025-11-12 10:42:41 +0700

    Update job_ex1.py

--- jobs/job_ex1.py
+++ jobs/job_ex1.py
@@ -4,5 +4,5 @@ class HelloWorldJob(Job):
     name = "Hello Job"
     description = "Một job đơn giản chỉ in ra thông tin"
 
-    def run(self, data, commit):
+    def run(self):
         self.log_info("Xin chào từ Nautobot! 👋")
