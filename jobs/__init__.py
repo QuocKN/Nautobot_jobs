45e6890aeff5dd83a95c30970d8312cbaaa3eaa3
Quoc Nguyen <quocnk2o4@gmail.com>
2025-11-12 10:07:10 +0700

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,5 +1,6 @@
 from nautobot.extras.jobs import register_jobs
-from .hello_world import HelloWorldJob
+from .job_ex1 import HelloWorldJob
 
 # Bắt buộc phải gọi register_jobs để Nautobot load Job
 register_jobs(HelloWorldJob)
+
